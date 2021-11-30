ebeaaf7207fa7ded622462f4c7ee91cfbb390bdf
Radwan Ahmed Anik <radwancse503@gmail.com>
2021-11-30 21:26:40 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+(function ($) {
+    "use strict";
+
+    $(".custom-progress").each(function () {
+
+        var value = $(this).attr('data-value');
+        var left = $(this).find('.progress-left .account-progress-bar');
+        var right = $(this).find('.progress-right .account-progress-bar');
+
+        if (value > 0) {
+            if (value <= 50) {
+                right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
+            } else {
+                right.css('transform', 'rotate(180deg)')
+                left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
+            }
+        }
+
+    })
+
+    function percentageToDegrees(percentage) {
+
+        return percentage / 100 * 360
+
+    }
+
+
+    $(document).ready(function () {
+        var url = window.location;
+        // Will only work if string in href matches with location
+        $('ul.nav a[href="' + url + '"]').parent().addClass('active');
+
+        // Will also work for relative and absolute hrefs
+        $('ul.nav a').filter(function () {
+            return this.href == url;
+        }).parent().addClass('active').parent().parent().addClass('active');
+    });
+
+
+})(jQuery);	
